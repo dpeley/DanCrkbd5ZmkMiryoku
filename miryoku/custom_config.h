13b37ba64b988f36bab2ae3e8ff4fb0431f23447
dpeley <dpeley@gmail.com>
2024-03-14 11:32:59 -0500

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -8,10 +8,10 @@
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
-XXX K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &kp LA(LG(K)) \
-XXX  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  XXX \
-&kp ENTER  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  &kp ENTER \
-               K32  K33  K34       K35  K36  K37
+XXX           K00            K01           K02           K03           K04                  K05           K06           K07           K08           K09           &kp LA(LG(K)) \
+XXX           K10            K11           K12           K13           K14                  K15           K16           K17           K18           K19           XXX           \
+&kp ENTER     K20            K21           K22           K23           K24                  K25           K26           K27           K28           K29           &kp ENTER     \
+                                           K32           K33           K34                  K35           K36           K37
 
 #define MIRYOKU_LAYERMAPPING_TAP( \
      K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
@@ -19,10 +19,10 @@ XXX  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  XXX \
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
-XXX K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  XXX \
-XXX  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  XXX \
-XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
-               K32  K33  K34       K35  K36  K37
+XXX           K00           K01           K02           K03           K04                 K05           K06           K07           K08           K09           XXX           \
+XXX           K10           K11           K12           K13           K14                 K15           K16           K17           K18           K19           XXX           \
+XXX           K20           K21           K22           K23           K24                 K25           K26           K27           K28           K29           XXX           \
+                                          K32           K33           K34                 K35           K36           K37
 
 #define MIRYOKU_LAYERMAPPING_NAV( \
      K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
@@ -30,10 +30,10 @@ XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
-&kp LC(A) K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  XXX \
-&kp BSPC  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  XXX \
-&kp ENTER  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
-               K32  K33  K34       K35  K36  K37
+&kp LC(A)     &kp LC(Z)     &kp LC(X)     &kp LC(C)     &kp LC(V)     &kp LC(Y)           K05           K06           K07           K08           K09           XXX           \
+&kp BSPC      K10           K11           K12           K13           K14                 K15           K16           K17           K18           K19           XXX           \
+&kp ENTER     K20           K21           &kp ENTER     K23           K24                 K25           K26           K27           K28           K29           XXX           \
+                                          K32           K33           K34                 K35           K36           K37
 
 #define MIRYOKU_LAYERMAPPING_MOUSE( \
      K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
@@ -41,10 +41,10 @@ XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
-XXX K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  XXX \
-XXX  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  XXX \
-XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
-               K32  K33  K34       K35  K36  K37
+XXX         &kp RC(RS(TAB)) &kp LA(LEFT)  &kp LA(RIGHT) &kp LC(TAB)   K04                 &kp LC(Y)     &kp LC(V)     &kp LC(C)     &kp LC(X)     &kp LC(Z)     XXX           \
+XXX           K10           K11           K12           K13           K14                 &kp LC(A)     K16           K17           K18           K19           XXX           \
+XXX           K20           K21           &kp ENTER     K23           K24                 &kp LC(LA(G)) K26           K27           K28           K29           XXX           \
+                                          K32           K33           K34                 K35           K36           K37
 
 #define MIRYOKU_LAYERMAPPING_MEDIA( \
      K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
@@ -52,10 +52,10 @@ XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
-XXX K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  XXX \
-XXX  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  XXX \
-XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
-               K32  K33  K34       K35  K36  K37
+XXX           K00           K01           K02           K03           K04                 K05           K06           K07           K08           K09           XXX           \
+XXX           K10           K11           K12           K13           K14                 K15           K16           K17           K18           K19           XXX           \
+XXX           K20           K21           K22           K23           K24                 K25           K26           K27           K28           K29           XXX           \
+                                          K32           K33           K34                 K35           K36           K37
 
 #define MIRYOKU_LAYERMAPPING_NUM( \
      K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
@@ -63,21 +63,21 @@ XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
-XXX K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  XXX \
-&kp BSPC  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  XXX \
-XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
-               K32  K33  K34       K35  K36  K37
-
+XXX           K00           K01           K02           K03           K04                 K05           K06           K07           K08           K09           XXX           \
+&kp BSPC      K10           K11           K12           K13           K14                 K15           K16           K17           K18           K19           XXX           \
+XXX           K20           K21           K22           K23           K24                 K25           K26           K27           K28           K29           XXX           \
+                                          K32           K33           K34                 K35           K36           K37
+					      
 #define MIRYOKU_LAYERMAPPING_SYM( \
      K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
      K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
-XXX K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  XXX \
-XXX  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  XXX \
-XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
-               K32  K33  K34       K35  K36  K37
+XXX           K00           K01           K02           K03           K04                 K05           K06           K07           K08           K09           XXX           \
+XXX           K10           K11           K12           K13           K14                 K15           K16           K17           K18           K19           XXX           \
+XXX           K20           K21           K22           K23           K24                 K25           K26           K27           K28           K29           XXX           \
+                                          K32           K33           K34                 K35           K36           K37
 
 #define MIRYOKU_LAYERMAPPING_FUN( \
      K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
@@ -85,10 +85,10 @@ XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
-XXX K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  XXX \
-XXX  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  XXX \
-XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
-               K32  K33  K34       K35  K36  K37
+XXX           K00           K01           K02           K03           K04                 K05           K06           K07           K08           K09           XXX           \
+XXX           K10           K11           K12           K13           K14                 K15           K16           K17           K18           K19           XXX           \
+XXX           K20           K21           K22           K23           K24                 K25           K26           K27           K28           K29           XXX           \
+                                          K32           K33           K34                 K35           K36           K37
 
 #define MIRYOKU_LAYERMAPPING_BUTTON( \
      K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
@@ -96,9 +96,9 @@ XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
-XXX K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  XXX \
-XXX  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  XXX \
-XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
-               K32  K33  K34       K35  K36  K37
+XXX           K00           K01           K02           K03           K04                 K05           K06           K07           K08           K09           XXX           \
+XXX           K10           K11           K12           K13           K14                 K15           K16           K17           K18           K19           XXX           \
+XXX           K20           K21           K22           K23           K24                 K25           K26           K27           K28           K29           XXX           \
+                                          K32           K33           K34                 K35           K36           K37
 
 #endif
