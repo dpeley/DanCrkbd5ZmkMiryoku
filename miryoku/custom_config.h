1c45ee9f23bb9c2756c0745404e852f95533310d
dpeley <dpeley@gmail.com>
2024-03-13 21:35:42 -0500

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,11 +1,8 @@
-// Copyright 2021 Manna Harbour
-// https://github.com/manna-harbour/miryoku
-
 #if defined (MIRYOKU_KEYBOARD_CORNE)
-#error
+
 #define XXX &none
 
-#define MIRYOKU_LAYOUTMAPPING_BASE( \
+#define MIRYOKU_LAYERMAPPING_BASE( \
      K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
      K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
@@ -16,7 +13,7 @@ XXX  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  XXX \
 &kp ENTER  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  &kp ENTER \
                K32  K33  K34       K35  K36  K37
 
-#define MIRYOKU_LAYOUTMAPPING_TAP( \
+#define MIRYOKU_LAYERMAPPING_TAP( \
      K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
      K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
@@ -27,7 +24,7 @@ XXX  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  XXX \
 XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
                K32  K33  K34       K35  K36  K37
 
-#define MIRYOKU_LAYOUTMAPPING_NAV( \
+#define MIRYOKU_LAYERMAPPING_NAV( \
      K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
      K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
@@ -38,7 +35,7 @@ XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
 &kp ENTER  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
                K32  K33  K34       K35  K36  K37
 
-#define MIRYOKU_LAYOUTMAPPING_MOUSE( \
+#define MIRYOKU_LAYERMAPPING_MOUSE( \
      K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
      K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
@@ -49,7 +46,7 @@ XXX  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  XXX \
 XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
                K32  K33  K34       K35  K36  K37
 
-#define MIRYOKU_LAYOUTMAPPING_MEDIA( \
+#define MIRYOKU_LAYERMAPPING_MEDIA( \
      K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
      K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
@@ -60,7 +57,7 @@ XXX  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  XXX \
 XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
                K32  K33  K34       K35  K36  K37
 
-#define MIRYOKU_LAYOUTMAPPING_NUM( \
+#define MIRYOKU_LAYERMAPPING_NUM( \
      K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
      K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
@@ -71,7 +68,7 @@ XXX K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  XXX \
 XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
                K32  K33  K34       K35  K36  K37
 
-#define MIRYOKU_LAYOUTMAPPING_SYM( \
+#define MIRYOKU_LAYERMAPPING_SYM( \
      K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
      K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
@@ -82,7 +79,7 @@ XXX  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  XXX \
 XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
                K32  K33  K34       K35  K36  K37
 
-#define MIRYOKU_LAYOUTMAPPING_FUN( \
+#define MIRYOKU_LAYERMAPPING_FUN( \
      K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
      K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
@@ -93,7 +90,7 @@ XXX  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  XXX \
 XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
                K32  K33  K34       K35  K36  K37
 
-#define MIRYOKU_LAYOUTMAPPING_BUTTON( \
+#define MIRYOKU_LAYERMAPPING_BUTTON( \
      K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
      K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
