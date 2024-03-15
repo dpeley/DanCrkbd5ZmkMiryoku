70f9a58f12612a0f79283b0d2890285aedadd86c
dpeley <dpeley@gmail.com>
2024-03-14 19:49:10 -0500

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -3,9 +3,10 @@
 
 // This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 2 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-//Aimee's Layers
-//nice_nano_v2
-//corne_left,corne_right
+//Dan's Layers
+//Board=nice_nano_v2
+//shield=corne_left,corne_right
+//Default layout (ColemakDH)
 
 /*
 #########################
@@ -21,6 +22,8 @@ Note that the block you're editing has no commas.
 
 ZMK keycodes can be found here: https://zmk.dev/docs/codes
 and must start with an action (&kp, or &bt, etc).
+
+Layer Lock is coming! https://github.com/ecstatic-morse/zmk/blob/molock/docs/docs/behaviors/layers.md#momentary-layer-lock
 */
 
 #if defined (MIRYOKU_KEYBOARD_CORNE)
@@ -33,9 +36,9 @@ and must start with an action (&kp, or &bt, etc).
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
-XXX           K00            K01           K02           K03           K04                  K05           K06           K07           K08           K09           &kp LA(LG(K)) \
+XXX           K00            K01           K02           K03           K04                  K05           K06           K07           K08           K09           XXX           \
 XXX           K10            K11           K12           K13           K14                  K15           K16           K17           K18           K19           XXX           \
-&kp ENTER     K20            K21           K22           K23           K24                  K25           K26           K27           K28           K29           &kp ENTER     \
+XXX           K20            K21           K22           K23           K24                  K25           K26           K27           K28           K29           XXX           \
                                            K32           K33           K34                  K35           K36           K37
 
 #define MIRYOKU_LAYERMAPPING_TAP( \
@@ -55,9 +58,9 @@ XXX           K20           K21           K22           K23           K24       
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
-&kp LC(A)     &kp LC(Z)     &kp LC(X)     &kp LC(C)     &kp LC(V)     &kp LC(Y)           &kp LC(F)   &kp RC(RS(TAB)) &kp LA(LEFT)  &kp LA(RIGHT) &kp LC(TAB)   XXX           \
-&kp BSPC      K10           K11           K12           K13           K14                 K15           K16           K17           K18           K19           XXX           \
-&kp ENTER     K20           K21           &kp ENTER     K23           K24                 K25           K26           K27           K28           K29           XXX           \
+XXX           &kp LC(Z)     &kp LC(X)     &kp LC(C)     &kp LC(V)     &kp LC(Y)           &kp LC(F)   &kp RC(RS(TAB)) &kp LA(LEFT)  &kp LA(RIGHT) &kp LC(TAB)   XXX           \
+XXX           K10           K11           K12           K13           &kp LC(A)           K15           K16           K17           K18           K19           XXX           \
+XXX           K20           &kp BSPC      &kp ENTER     K23           K24                 K25           K26           K27           K28           K29           XXX           \
                                           K32           K33           K34                 K35           K36           K37
 
 #define MIRYOKU_LAYERMAPPING_MOUSE( \
@@ -68,7 +71,7 @@ XXX           K20           K21           K22           K23           K24       
 ) \
 XXX         &kp RC(RS(TAB)) &kp LA(LEFT)  &kp LA(RIGHT) &kp LC(TAB)   K04                 &kp LC(Y)     &kp LC(V)     &kp LC(C)     &kp LC(X)     &kp LC(Z)     XXX           \
 XXX           K10           K11           K12           K13           K14                 &kp LC(A)     K16           K17           K18           K19           XXX           \
-XXX           K20           K21           &kp ENTER     K23           K24                 &kp LC(LA(G)) K26           K27           K28           K29           XXX           \
+XXX           K20           &kp BSPC      &kp ENTER     K23           K24                 &kp LC(LA(G)) K26           K27           K28           K29           XXX           \
                                           K32           K33           K34                 K35           K36           K37
 
 #define MIRYOKU_LAYERMAPPING_MEDIA( \
@@ -88,8 +91,8 @@ XXX           K20           K21           K22           K23           K24       
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
-&molock       K00           K01           K02           K03           K04                 K05           K06           K07           K08           K09           XXX           \
-&kp BSPC      K10           K11           K12           K13           K14                 K15           K16           K17           K18           K19           XXX           \
+XXX           K00           K01           K02           K03           K04                 K05           K06           K07           K08           K09           XXX           \
+XXX           K10           K11           K12           K13           K14                 K15           K16           K17           K18           K19           XXX           \
 XXX           K20           K21           K22           K23           K24                 K25           K26           K27           K28           K29           XXX           \
                                           K32           K33           K34                 K35           K36           K37
 					      
