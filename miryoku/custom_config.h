f361c15ce3a3ad0e03ec033f24804e106fd50355
dpeley <dpeley@gmail.com>
2024-03-14 19:56:38 -0500

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -4,9 +4,9 @@
 // This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 2 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 //Aimee's Layers
-//nice_nano_v2
-//corne_left,corne_right
-//Qwerty
+//Board=nice_nano_v2
+//shield=corne_left,corne_right
+//Qwerty layout
 
 /*
 #########################
@@ -22,6 +22,9 @@ Note that the block you're editing has no commas.
 
 ZMK keycodes can be found here: https://zmk.dev/docs/codes
 and must start with an action (&kp, or &bt, etc).
+
+Layer Lock is coming! https://github.com/ecstatic-morse/zmk/blob/molock/docs/docs/behaviors/layers.md#momentary-layer-lock
+*/
 */
 
 #if defined (MIRYOKU_KEYBOARD_CORNE)
@@ -89,7 +92,7 @@ XXX           K20           K21           K22           K23           K24       
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
-&molock       K00           K01           K02           K03           K04                 K05           K06           K07           K08           K09           XXX           \
+XXX           K00           K01           K02           K03           K04                 K05           K06           K07           K08           K09           XXX           \
 &kp BSPC      K10           K11           K12           K13           K14                 K15           K16           K17           K18           K19           XXX           \
 XXX           K20           K21           K22           K23           K24                 K25           K26           K27           K28           K29           XXX           \
                                           K32           K33           K34                 K35           K36           K37
